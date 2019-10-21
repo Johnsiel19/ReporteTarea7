f8ecbdf51f9a945fdbbb5277188121c003c79fe3
Johnsiel Castaños <50462377+Johnsiel19@users.noreply.github.com>
2019-10-21 00:59:51 -0300

    subiendo DAL

---
+++ DAL/Contexto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+
+
+        public DbSet<Pagos> Pagos { get; set; }
+        public DbSet<Analisis> Analisis { get; set; }
+        public DbSet<Pacientes> Pacientes { get; set; }
+        public DbSet<TipoAnalisis> TipoAnaliss { get; set; }
+
+
+        public Contexto() : base("ConStr")
+        { }
+
+    }
+}
